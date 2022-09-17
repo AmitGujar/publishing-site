181c490baf806f882d5592810adc1c98d68cd4c8
AmitGujar <amithero3342@gmail.com>
2022-09-17 15:41:26 +0530

    login ui

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
-const dotenv = require('dotenv').config()
+const dotenv = require("dotenv").config();
 
 //db
-const connectDB = require('./src/configs/db');
-
+const connectDB = require("./src/configs/db");
 connectDB();
+
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
 
 module.exports = app;
