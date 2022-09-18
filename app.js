a86b0ed1f8397fe1769dbba27bb4e122358416a6
AmitGujar <amithero3342@gmail.com>
2022-09-18 07:27:19 +0530

    crud added

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
-
+const methodOverride = require("method-override");
+const articleRouter = require("./src/routes/articles"); 
+const Article = require("./src/models/article");
 //db
 const connectDB = require("./src/configs/db");
 connectDB();
-
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.json());
+app.use(methodOverride('_method'))
 
-app.get("/", (req, res) => {
-  res.render("login");
+
+app.use('/articles',articleRouter)
+
+
+
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: 'desc'
+  })
+  res.render("index", {articles: articles});
 });
 
 module.exports = app;
--- src/configs/db.js
+++ src/configs/db.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URL)
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`Connected to Database = ${conn.connection.host}`)
     } catch (error) {
         console.log(error)
         process.exit(1)
